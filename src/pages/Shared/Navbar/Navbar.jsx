01d8d3982779531aae021b3c8af1659ab5349f53
rasel <shoaibullahrasel@gmail.com>
2024-05-06 02:42:38 +0600

    added navbar and banner part

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,37 +1,51 @@
 import React from "react";
 
 const Navbar = () => {
-  const options = (
+  const navOption = (
     <>
       <li>
-        <a>Item 1</a>
+        <a>Home</a>
       </li>
       <li>
-        <a>Parent</a>
+        <a>Contact Us</a>
       </li>
       <li>
-        <a>Item 3</a>
+        <a>Our Menu</a>
       </li>
     </>
   );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white">
         <div className="navbar-start">
           <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-                {options}
+              {navOption}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">ZeRo Restaurant</a>
+          <a className="btn btn-ghost normal-case text-xl">ZeRo Restaurant</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            {options}
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navOption}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
