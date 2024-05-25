47ce85244f1f8c1ed1587d832d984dba9fc44d8a
rasel <shoaibullahrasel@gmail.com>
2024-05-25 17:02:27 +0600

    added the google auth in the login page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { router } from "./Routes/router";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-2xl mx-auto">
-        <RouterProvider router={router}></RouterProvider>
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-2xl mx-auto">
+          <RouterProvider router={router}></RouterProvider>
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  loadCaptchaEnginge,
-  LoadCanvasTemplate,
-  validateCaptcha,
-} from "react-simple-captcha";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import {loadCaptchaEnginge, LoadCanvasTemplate,validateCaptcha, } from "react-simple-captcha";
+import { AuthContext } from "../../providers/AuthProvider";
+
 
 const Login = () => {
   const captchaRef = useRef(null);
   const [disabled, setDisabled] = useState(true);
+
+  const {signIn} = useContext(AuthContext);
 
   useEffect(() => {
     loadCaptchaEnginge(6);
@@ -19,7 +19,13 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
+    signIn(email , password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
   };
+
   const handleValidateCaptcha = () => {
     const user_captcha_value = captchaRef.current.value;
 
